320ed7282d20217b63c4795e0592c47a91b2d242
Avery Smart <59900478+Averysmart@users.noreply.github.com>
2020-01-21 19:24:43 -0500

    Update and rename README.md to git-lab-program.cc

---
+++ git-lab-program.cc
@@ -0,0 +1,20 @@
+# git-lab-2
+/*
+ *        File: git-lab-program.cc
+ *      Author: Avery Smart
+ *        Date: 01/21/2020
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+  cout << "Hello Git!!" << end1;
+
+    /*add code*/
+    return 0;
+}// main
